0bcfa0d89dabeb98688fbbd31280c72c57a219b7
Alejandro Gamarra <alejandro.gamarra.t@gmail.com>
2022-06-14 19:50:09 +0200

    Version 1.1
    - Deleted unused classes
    - Renamed a package

--- Songs/src/main/java/com/songs/repository/SongsRepository.java
+++ Songs/src/main/java/com/songs/repository/SongsRepository.java
@@ -1,4 +1,4 @@
-package com.songs.dao;
+package com.songs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- Songs/src/main/java/com/songs/service/SongsService.java
+++ Songs/src/main/java/com/songs/service/SongsService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.songs.dao.SongsRepository;
 import com.songs.model.Song;
+import com.songs.repository.SongsRepository;
 
 @Service
 public class SongsService {
